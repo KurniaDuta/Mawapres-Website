3a9b06c49220483309554c9674c92c42ef1b5122
VozSoldat <muhamaderril7@gmail.com>
2024-12-09 23:02:12 +0700

    🩹 Fix: fix problem on `mahasiswa/riwayat_lihat.php`

--- resources/views/mahasiswa/riwayat_lihat.php
+++ resources/views/mahasiswa/riwayat_lihat.php
@@ -33,10 +33,134 @@ $dataToShow = [
             <?php echo $title; ?>
         </h4>
         <main class="bg-white flex-1 m-8 rounded-xl p-8 flex flex-col gap-8 ml-[340px]">
-            <?php
-            // sementara. supaya mode for-reader
-            $isAdmin = true;
-            include "../modular_submisi.php" ?>
+
+            <span class="flex flex-col gap-2">
+                <div class="flex">
+                    <label for="nama_kompetisi" class="">Nama Kompetisi</label>
+                </div>
+                <p class="text-sm text-gray-400">Nama kompetisi yang tertera di dokumen-dokumen bukti.</p>
+
+                <section class="p-2 h-14 w-full [box-shadow:0em_0.3em_0.3em#bcbcbc] rounded-lg flex items-center">
+                    <?php echo $dataToShow['nama_kompetisi'] ?>
+                </section>
+
+            </span>
+            <span class="flex flex-1 gap-16">
+                <div class="flex flex-col flex-1">
+                    <div class="flex">
+                        <label for="tanggal_mulai" class="">Tanggal Mulai</label>
+                    </div>
+                    <p class="text-sm text-gray-400">Tanggal kompetisi dimulai.</p>
+                    <section class="p-2 h-14 w-full [box-shadow:0em_0.3em_0.3em#bcbcbc] rounded-lg flex items-center">
+                        <?php echo $dataToShow['tanggal_mulai'] ?>
+                    </section>
+
+                </div class="flex flex-col">
+                <div class="flex flex-col flex-1">
+                    <div class="flex">
+                        <label for="tanggal_selesai" class="">Tanggal Selesai</label>
+                    </div>
+                    <p class="text-sm text-gray-400">Tanggal kompetisi selesai.</p>
+
+                        <section class="p-2 h-14 w-full [box-shadow:0em_0.3em_0.3em#bcbcbc] rounded-lg flex items-center">
+                            <?php echo $dataToShow['tanggal_selesai'] ?>
+                        </section>
+
+                </div>
+            </span>
+            <span class="flex flex-col gap-2">
+                <div class="flex">
+                    <label for="penyelenggara" class="">Penyelenggara</label>
+                </div>
+                <p class="text-sm text-gray-400">Tulis penyelenggara yang tertera pada dokumen-dokumen bukti.</p>
+
+                <!-- input or show -->
+                    <section class="p-2 h-14 w-full [box-shadow:0em_0.3em_0.3em#bcbcbc] rounded-lg flex items-center">
+                        <?php echo $dataToShow['penyelenggara'] ?>
+                    </section>
+            </span>
+            <span class="flex flex-col gap-2">
+                <div class="flex">
+                    <label for="jenis_kompetisi" class="">Jenis Kompetisi</label>
+                </div>
+                <p class="text-sm text-gray-400">Pilih jenis di bawah yang paling cocok dengan kompetisimu.</p>
+                    <section class="p-2 h-14 w-full [box-shadow:0em_0.3em_0.3em#bcbcbc] rounded-lg flex items-center">
+                        <?php echo $dataToShow['jenis_kompetisi'] ?>
+                    </section>
+            </span>
+            <span class="flex flex-col gap-2">
+                <div class="flex">
+                    <label for="tingkat_kompetisi" class="">Tingkat Kompetisi</label>
+                </div>
+                <p class="text-sm text-gray-400">Pilih tingkat di bawah yang paling cocok dengan kompetisimu.</p>
+
+                    <section class="p-2 h-14 w-full [box-shadow:0em_0.3em_0.3em#bcbcbc] rounded-lg flex items-center">
+                        <?php echo $dataToShow['tingkat_kompetisi'] ?>
+                    </section>
+            </span>
+            <span class="flex flex-col gap-2">
+                <div class="flex">
+                    <label for="juara_kompetisi" class="">Juara Kompetisi</label>
+                </div>
+                <p class="text-sm text-gray-400">Pilih juara di bawah yang paling cocok dengan kompetisimu.</p>
+                    <section class="p-2 h-14 w-full [box-shadow:0em_0.3em_0.3em#bcbcbc] rounded-lg flex items-center">
+                        <?php echo $dataToShow['juara_kompetisi'] ?>
+                    </section>
+            </span>
+
+            <span class="flex flex-col gap-2">
+                <div class="flex">
+                    <h4 class="">URL Kompetisi</h4>
+                </div>
+                <p class="text-sm text-gray-400">Tulis URL publikasi penyelenggara kompetisi.</p>
+
+                    <section class="p-2 h-14 w-full [box-shadow:0em_0.3em_0.3em#bcbcbc] rounded-lg flex items-center">
+                        <?php echo $dataToShow['url_kompetisi'] ?>
+                    </section>
+            </span>
+
+            <span class="flex flex-col gap-2">
+                <div class="flex">
+                    <h4 class="">Jumlah Peserta</h4>
+                </div>
+                <p class="text-sm text-gray-400">Tulis jumlah peserta yang mengikuti kompetisi.</p>
+
+                    <section class="p-2 h-14 w-full [box-shadow:0em_0.3em_0.3em#bcbcbc] rounded-lg flex items-center">
+                        <?php echo $dataToShow['jumlah_peserta'] ?>
+                    </section>
+            </span>
+
+            <span class="flex flex-col gap-2">
+                <div class="flex">
+                    <label for="jumlah_pt" class="">Jumlah PT</label>
+                </div>
+                <p class="text-sm text-gray-400">Tulis jumlah perguruan tinggi yang mengikuti kompetisi.</p>
+                    <section class="p-2 h-14 w-full [box-shadow:0em_0.3em_0.3em#bcbcbc] rounded-lg flex items-center">
+                        <?php echo $dataToShow['jumlah_pt'] ?>
+                    </section>
+            </span>
+
+            <span class="flex flex-col gap-2">
+                <div class="flex">
+                    <label for="nomor_surat_tugas" class="">Nomor Surat Tugas</label>
+                </div>
+                <p class="text-sm text-gray-400">Tulis nomor surat tugas.</p>
+                    <section class="p-2 h-14 w-full [box-shadow:0em_0.3em_0.3em#bcbcbc] rounded-lg flex items-center">
+                        <?php echo $dataToShow['nomor_surat_tugas'] ?>
+                    </section>
+            </span>
+
+            <span class="flex flex-col gap-2">
+                <div class="flex">
+                    <label for="tanggal_surat_tugas" class="">Tanggal Surat Tugas</label>
+                </div>
+                <p class="text-sm text-gray-400">Tanggal surat tugas diberikan.</p>
+                    <section class="p-2 h-14 w-full [box-shadow:0em_0.3em_0.3em#bcbcbc] rounded-lg flex items-center">
+                        <?php echo $dataToShow['tanggal_surat_tugas'] ?>
+                    </section>
+            </span>
+
+
             <span class="flex flex-col gap-2">
                 <div class="flex">
                     <label class="">Keanggotaan</label>
@@ -121,20 +245,15 @@ $dataToShow = [
                     </svg>
 
                 </div>
-                
+
             </span>
             <span class="text-red-500 flex justify-between">
-                <?php if (!$isAdmin) : ?>
-                    <p class="w-36">
-                        *Wajib diisi.
-                    </p>
-                <?php endif; ?>
                 <div class="flex flex-row-reverse gap-3 w-full">
-                    <?php if ($dataToShow['status']==Status::Ditolak) :?>
-                    <button class="flex rounded-md  w-fit py-3 px-6 text-white bg-[#5088DA] [box-shadow:0em_0.3em_0.3em#bcbcbc]">
-                        <p>EDIT</p>
-                    </button>
-                    <?php endif;?>
+                    <?php if ($dataToShow['status'] == Status::Ditolak) : ?>
+                        <button class="flex rounded-md  w-fit py-3 px-6 text-white bg-[#5088DA] [box-shadow:0em_0.3em_0.3em#bcbcbc]">
+                            <p>EDIT</p>
+                        </button>
+                    <?php endif; ?>
                     <button class="flex rounded-md  w-fit py-3 px-6 text-white bg-gray-400 [box-shadow:0em_0.3em_0.3em#bcbcbc]">
                         <p>KEMBALI</p>
                     </button>
